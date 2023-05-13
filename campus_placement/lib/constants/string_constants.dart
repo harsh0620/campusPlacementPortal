721581f50a866f849116b590a5df994a9f644449
Nishtha <nishtha.s@gida.io>
2023-05-13 18:13:28 +0530

    added required strings

---
+++ campus_placement/lib/constants/string_constants.dart
@@ -0,0 +1,27 @@
+class StringConst {
+  static const continueReset = 'Continue';
+  static const enterCode = 'Confirm your code';
+  static const enterCodeSubText = 'This code helps to reset password';
+  static const enter = 'Enter';
+  static const enterNewPassword = 'Enter new password';
+  static const resetPassword = 'Reset Password';
+  static const forgotPassword = 'Forgot Password?';
+
+  static const login = 'Welcome back!';
+  static const loginSubText = 'Enter your credentials to access your account';
+  static const emailSubText = 'Enter your email here...';
+  static const forgotPassEmailText = 'Enter your email to recieve a link';
+  static const sentResetLink = 'Send Reset link';
+  static const emailText = 'Email Address';
+  static const passwordText = 'Enter your password here...';
+  static const confirmPassword = 'Confirm Password';
+  static const confirmOTP = 'Confirm and Continue';
+  static const invalidCredentialsText = 'Invalid credentials';
+  static const resetPasswordError =
+      'Password and Confirm Password are not same';
+  static const signIn = 'Sign In';
+  static const noAccount = 'Dont have an account?';
+  static const createAccount = 'Create Account';
+  static const otpError = 'Invalid otp';
+  static const submitText = 'Submit';
+}
