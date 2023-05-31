d766ada72e18a307f979b747dc2234781c896e45
Harsh Chandravanshi <Harsh.Chandravanshi@securemeters.com>
2023-05-31 21:19:14 +0530

    added some comments,but now leaving it to make report

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,28 +1,28 @@
-import express from "express";
-const router = express.Router();
+import express from "express"; // Importing express module
+const router = express.Router(); // Creating a router instance
 import {
-  createAdmin,
-  updateAdmin,
-  getAdminProfileDetails,
-  createCompany,
-} from "../controller/adminAuthController.js";
+  createAdmin, // Function to create an admin user
+  updateAdmin, // Function to update an existing admin user's profile
+  getAdminProfileDetails, // Function to get admin user's profile details
+  createCompany, // Function to create a new company user
+} from "../controller/adminAuthController.js"; // Importing functions from adminAuthController.js file
 import {
-  verifyJobDrive,
-  verifyStudent,
-  sendMailToGoogleGroups,
-  sendMessagesOnMail,
-  getStudents,
-  getStudentById,
-  sendMailToSelectedStudent,
-  sendMailToUsers,
-  getCompany,
-  getCompanyById,
-  getJob,
-  getJobById,
-  getAppliedStudents,
-  getStats,
-  getStudentByIdInCSV,
-} from "../controller/adminFeaturesController.js";
+  verifyJobDrive, // Function to verify job drive
+  verifyStudent, // Function to verify student
+  sendMailToGoogleGroups, // Function to send mails to google groups
+  sendMessagesOnMail, // Function to send messages on mail
+  getStudents, // Function to get students
+  getStudentById, // Function to get student by id
+  sendMailToSelectedStudent, // Function to send mail to selected student
+  sendMailToUsers, // Function to send mail to users
+  getCompany, // Function to get company
+  getCompanyById, // Function to get company by id
+  getJob, // Function to get job
+  getJobById, // Function to get job by id
+  getAppliedStudents, // Function to get applied students
+  getStats, // Function to get stats
+  getStudentByIdInCSV, // Function to get student by id in CSV
+} from "../controller/adminFeaturesController.js"; // Importing functions from adminFeaturesController.js file
 
 /**
  * @desc Update an existing admin user's profile
@@ -88,8 +88,9 @@ router.route("/job").get(getJob);
  * @access Private
  * */
 router.route("/job/:jobId").get(getJobById);
+
 /**
- * Get job by id
+ * Get applied students
  * @route GET /api/v1/admin/job/appliedBy/:jobId
  * @access Private
  * */
@@ -101,44 +102,54 @@ router.route("/job/appliedBy/:jobId").get(getAppliedStudents);
  * @access Private
  * */
 router.route("/sendMail/:studentId").post(sendMailToSelectedStudent);
+
 /**
  * Send mail to selected student
  * @route POST /api/v1/admin/sendMail
  * @access Private
  * */
 router.route("/sendMail").post(sendMailToUsers);
+
 /**
- * Verify a student
- *
+ * Verify student
  * @route PATCH /api/v1/admin/verifyStudent/:studentId
  * @access Private
- */
+ * */
 router.route("/verifyStudent/:studentId").patch(verifyStudent);
 
 /**
- * Verify a job drive
- *
+ * Verify jobDrive
  * @route PATCH /api/v1/admin/verifyJobDrive/:jobDriveId
  * @access Private
- */
+ * */
 router.route("/verifyJobDrive/:jobDriveId").patch(verifyJobDrive);
 
 /**
- * Send mails to Google groups
- *
+ * Send mail to google groups
  * @route POST /api/v1/admin/sendMails/:jobDriveId
  * @access Private
- */
+ * */
 router.route("/sendMails/:jobDriveId").post(sendMailToGoogleGroups);
 
 /**
- * Send messages
- *
- * @route POST /api/v1/admin/sendMessages
+ * Send messages on mail
+ * @route POST /api/v1/admin/sendMessagesOnMail
  * @access Private
- */
+ * */
 router.route("/sendMessagesOnMail").post(sendMessagesOnMail);
+
+/**
+ * Get stats
+ * @route GET /api/v1/admin/stats
+ * @access Private
+ * */
 router.route("/stats").get(getStats);
+
+/**
+ * Get student by id in csv
+ * @route POST /api/v1/admin/csv
+ * @access Private
+ * */
 router.route("/csv").post(getStudentByIdInCSV);
 
 export default router;
